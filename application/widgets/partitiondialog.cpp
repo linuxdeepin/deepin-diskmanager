1b22683eaac99b7d9ba61f8b74f775718a6ffd8b
yuandandangit <yuandandan@uniontech.com>
2020-09-11 10:32:40 +0800

    fix: 确认是否分区提示窗口代码规范
    进行代码规范
    
    Log: 确认是否分区提示窗口代码规范
    Change-Id: Ib219aad52afa86e0e89dfa8955a882f44d2a850f
    Reviewed-on: http://gerrit.uniontech.com/c/beijing/deepin-diskmanager/+/4374
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: denglinglong <denglinglong@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- application/widgets/partitiondialog.cpp
+++ application/widgets/partitiondialog.cpp
@@ -1,27 +1,40 @@
-/*
-* Copyright (C) 2019 ~ 2020 Deepin Technology Co., Ltd.
-*
-* Author:     zhangkai <zhangkai@uniontech.com>
-* Maintainer: zhangkai <zhangkai@uniontech.com>
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* any later version.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ * @copyright 2020-2020 Uniontech Technology Co., Ltd.
+ *
+ * @file partitiondialog.h
+ *
+ * @brief 是否创建分区选择窗口
+ *
+ * @date 2020-09-08 14:56
+ *
+ * Author: yuandandan  <yuandandan@uniontech.com>
+ *
+ * Maintainer: yuandandan  <yuandandan@uniontech.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "partitiondialog.h"
 #include "partedproxy/dmdbushandler.h"
 #include "partitioninfo.h"
+
 #include <DLabel>
 #include <DFrame>
 #include <DFontSizeManager>
+
 #include <QVBoxLayout>
 #include <QDebug>
+
 PartitionDialog::PartitionDialog(QWidget *parent)
     : DDBase(parent)
 {

bdb4209d34720804e26829ea6500297bfe91c6e8
ut001736 <liweigang@uniontech.com>
2020-10-30 16:27:38 +0800

    fix: 修改根目录下生成0文件的bug
    修改根目录下生成0文件的问题
    
    Log: 修改重启系统后会默认在根目录下生成0文件的问题
    Change-Id: Ie9a6ebc2d3ce1043cfe0e1791826ada8f1255805
    Reviewed-on: http://gerrit.uniontech.com/c/beijing/deepin-diskmanager/+/9768
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: denglinglong <denglinglong@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- service/udev/USBadd.sh
+++ service/udev/USBadd.sh
@@ -3,7 +3,7 @@
 PROC_NAME='deepin-diskmanager'
 
 ProcNumber=`pidof $PROC_NAME`
-if [ $ProcNumber > 0 ];then
+if [ $ProcNumber ];then
    /usr/bin/dbus-send --system --type=method_call --dest=com.deepin.diskmanager /com/deepin/diskmanager com.deepin.diskmanager.updateUsb
 else
    exit		

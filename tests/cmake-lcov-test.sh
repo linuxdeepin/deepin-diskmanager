3434965783343c4d2879bc2b2aa4ad6da5c5cd51
bulongwei@uniontech.com <bulongwei@uniontech.com>
2021-05-10 19:33:50 +0800

    fix: 关闭自动开启内存检测 更改服务器密码
    关闭自动开启内存检测 更改服务器密码
    
    Log: 关闭自动开启内存检测 更改服务器密码
    Change-Id: I3444f24a64af5f4d71367dce085d886ac8f14875

--- tests/cmake-lcov-test.sh
+++ tests/cmake-lcov-test.sh
@@ -2,7 +2,7 @@
 export DISPLAY=:0.0
 utdir=build-ut
 rm -r $utdir
-echo "123456" | sudo -S rm -r ../$utdir
+echo "Uos123!!" | sudo -S rm -r ../$utdir
 mkdir ../$utdir
 cd ../$utdir
 
@@ -10,22 +10,22 @@ cmake -DCMAKE_SAFETYTEST_ARG="CMAKE_SAFETYTEST_ARG_ON" ..
 make -j4
 
 touch ./tests/utcase.log
-echo "123456" | sudo -S ./tests/deepin-diskmanager_app_test --gtest_output=xml:./report/report.xml | tee ./bin/utcase.log
+echo "Uos123!!" | sudo -S ./tests/deepin-diskmanager_app_test --gtest_output=xml:./report/report.xml | tee ./bin/utcase.log
 
 workdir=$(cd ../$(dirname $0)/$utdir; pwd)
 
 mkdir -p report
 
-echo "123456" | sudo -S lcov -d $workdir -c -o ./report/coverage.info
+echo "Uos123!!" | sudo -S lcov -d $workdir -c -o ./report/coverage.info
 
-echo "123456" | sudo -S lcov --extract ./report/coverage.info '*/application/*' '*/basestruct/*' -o ./report/coverage.info
+echo "Uos123!!" | sudo -S lcov --extract ./report/coverage.info '*/application/*' '*/basestruct/*' -o ./report/coverage.info
 
-echo "123456" | sudo -S lcov --remove ./report/coverage.info '*/tests/*' '*/application/partedproxy/*' -o ./report/coverage.info
+echo "Uos123!!" | sudo -S lcov --remove ./report/coverage.info '*/tests/*' '*/application/partedproxy/*' -o ./report/coverage.info
 
-echo "123456" | sudo -S genhtml -o ./report ./report/coverage.info
+echo "Uos123!!" | sudo -S genhtml -o ./report ./report/coverage.info
 
-echo "123456" | sudo -S chmod 666 ./asan.log*
+echo "Uos123!!" | sudo -S chmod 666 ./asan.log*
 
-echo "123456" | sudo -S chmod 666 ./report/index.html
+echo "Uos123!!" | sudo -S chmod 666 ./report/index.html
 
 exit 0

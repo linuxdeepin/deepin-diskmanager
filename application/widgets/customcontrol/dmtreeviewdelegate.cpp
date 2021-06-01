eedf873e2cefc5a14c450ad24fead0e0ca2e66e7
yuandandangit <yuandandan@uniontech.com>
2021-06-01 21:21:28 +0800

    fix: 解决设备树字符重叠显示的问题
    解决设备树字符重叠显示的问题
    
    Log: 解决设备树字符重叠显示的问题
    Bug: https://pms.uniontech.com/zentao/bug-view-81881.html
    Change-Id: Ie7e827b613b43f963b595378b1c62ec8b29d25e3

--- application/widgets/customcontrol/dmtreeviewdelegate.cpp
+++ application/widgets/customcontrol/dmtreeviewdelegate.cpp
@@ -158,11 +158,12 @@ void DmTreeviewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
         painter->setFont(font);
 
         // 获取文本宽度
-        QFontMetrics fmCapacity = painter->fontMetrics();
-        int textWidth = fmCapacity.width(text);
+//        QFontMetrics fmCapacity = painter->fontMetrics();
+//        int textWidth = fmCapacity.width(text);
 
-        textRect.setRect(paintRect.left() + 60, paintRect.top() + 5, textWidth + 1, 100);
-        painter->drawText(textRect, text);
+        textRect.setRect(paintRect.left() + 60, paintRect.top() + 5, 110, 100);
+        QString devName = painter->fontMetrics().elidedText(text, Qt::ElideMiddle, 108);
+        painter->drawText(textRect, devName);
         QColor text1color = m_parentPb.color(DPalette::Normal, DPalette::TextTips);
         painter->setPen(text1color);
         font = DFontSizeManager::instance()->get(DFontSizeManager::T8);
@@ -201,7 +202,7 @@ void DmTreeviewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
             painter->setPen(textColor);
             painter->setFont(font);
             textRect.setRect(paintRect.left() + 65, paintRect.top() + 8, 106, 100);
-            QString text = painter->fontMetrics().elidedText(text2, Qt::ElideMiddle, textRect.width());
+            QString text = painter->fontMetrics().elidedText(text2, Qt::ElideMiddle, 104);
             painter->drawText(textRect, text);
             QColor text1Color = m_parentPb.color(DPalette::Normal, DPalette::HighlightedText);
             painter->setPen(text1Color);
@@ -214,7 +215,7 @@ void DmTreeviewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &op
             painter->setPen(textColor);
             painter->setFont(font);
             textRect.setRect(paintRect.left() + 65, paintRect.top() + 8, 106, 100);
-            QString text = painter->fontMetrics().elidedText(text2, Qt::ElideMiddle, textRect.width());
+            QString text = painter->fontMetrics().elidedText(text2, Qt::ElideMiddle, 104);
             painter->drawText(textRect, text);
             QColor text1Color = m_parentPb.color(DPalette::Normal, DPalette::TextTips);
             painter->setPen(text1Color);

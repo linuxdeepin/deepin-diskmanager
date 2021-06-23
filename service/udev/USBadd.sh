4a4a6e5a193b96c32eee1ea8e384afc17069bde6
chenenlong <chenenlong@uniontech.com>
2021-06-23 16:45:14 +0800

    fix: udev脚本用的pidof命令有时会获取不到pid，导致问题。更换取得pid的方法
    udev脚本用的pidof命令有时会获取不到pid，导致问题。更换取得pid的方法
    
    Log: udev脚本用的pidof命令有时会获取不到pid，导致问题。更换取得pid的方法
    Change-Id: I5966d3cd465e32487258792129aa7d0133a58af3

--- service/udev/USBadd.sh
+++ service/udev/USBadd.sh
@@ -2,8 +2,9 @@
 
 PROC_NAME='deepin-diskmanager'
 
-ProcNumber=`pidof $PROC_NAME`
-if [ $ProcNumber ];then
+#ProcNumber=`pidof $PROC_NAME`
+ProcNumber=`ps aux |grep deepin-diskmanager-service |sed '/grep/d' |sed '/tail/d'`
+if [ -n "$ProcNumber" ];then
    /usr/bin/dbus-send --system --type=method_call --dest=com.deepin.diskmanager /com/deepin/diskmanager com.deepin.diskmanager.updateUsb
 else
    exit		

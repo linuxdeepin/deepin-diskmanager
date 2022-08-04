114e81eaf58ab9672bd5d4c538d747bc41ac6f40
guonafu <106046899+guonafu@users.noreply.github.com>
2022-08-04 13:57:19 +0800

    优化USB热插拔刷新问题 (#14) (#31)
    * feat: 优化USB热插拔刷新问题
    
    非存储设备USB热插拔时，磁盘不做刷新
    
    Log: 优化USB热插拔刷新问题
    
    Co-authored-by: ut001275 <107104713+ut001275@users.noreply.github.com>

--- service/udev/USBremove.sh
+++ service/udev/USBremove.sh
@@ -7,6 +7,6 @@ ProcNumber=`ps aux |grep deepin-diskmanager-service |sed '/grep/d' |sed '/tail/d
 if [ -n "$ProcNumber" ];then
    /usr/bin/dbus-send --system --type=method_call --dest=com.deepin.diskmanager /com/deepin/diskmanager com.deepin.diskmanager.updateUsbRemove
 else
-   exit		
+   exit 0
 fi
 

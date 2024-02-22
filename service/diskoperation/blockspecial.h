9a0c49410ac7887ec19603eb4fc2fc1483dcc3d4
rong wang <wangrong@uniontech.com>
2024-02-22 13:48:38 +0800

    doc: Update copyright
    As title.
    
    Log: update copyright

--- service/diskoperation/blockspecial.h
+++ service/diskoperation/blockspecial.h
@@ -1,6 +1,19 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: GPL-3.0-only
+/* modified by Uniontech Software Technology Co., Ltd. */
+/* Copyright (C) 2016 Mike Fleetwood
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef BLOCKSPECIAL_H
 #define BLOCKSPECIAL_H

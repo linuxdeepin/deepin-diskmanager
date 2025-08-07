d72ad07993a6f47dee66b75e4625b2f52c8b3c74
Tian Shilin <tianshilin@uniontech.com>
2025-08-07 21:12:44 +0800

    fix: update translations error
    log:

---
+++ application/translations/policy/policy_lo.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+	<name>policy</name>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-diskmanager!message" line="0"/>
+		<source>Authentication is required to read disk information</source>
+		<translation>ຕ້ອງການການຢືນຢັນເພື່ອອ່ານຂໍ້ມູນດິດ</translation>
+	</message>
+	<message>
+		<location filename="com.deepin.pkexec.deepin-diskmanager!description" line="0"/>
+		<source>Run Disk Manager Need Authentication</source>
+		<translation>ການເຮັດວຽກກັບ Disk Manager ຕ້ອງການການຢືນຢັນ</translation>
+	</message>
+</context>
+</TS>
